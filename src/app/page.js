16ba0bfbe3583a85fc97463baa8eebc3d8460051
Casey <dinnerdoggy@gmail.com>
2025-06-07 11:35:54 -0500

    auth setu

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 'use client';
 
- // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
 
-import { Button } from 'react-bootstrap';
-import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
 import { useAuth } from '@/utils/context/authContext';
 
 function Home() {
@@ -20,10 +18,6 @@ function Home() {
       }}
     >
       <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
     </div>
   );
 }
