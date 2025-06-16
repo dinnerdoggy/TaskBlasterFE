daf5d9d1cc149e0ef3a05e98a6f50d6651e5bc12
Casey <dinnerdoggy@gmail.com>
2025-06-15 19:39:03 -0500

    routing for edit duty established

--- src/components/DutyCard.js
+++ src/components/DutyCard.js
@@ -1,11 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import Card from 'react-bootstrap/Card';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/navigation';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const onDelete = () => {
+  console.log('Delete clicked');
+};
 
 function DutyCard({ dutyObj }) {
+  const router = useRouter();
+
+  const handleEdit = () => {
+    router.push(`/category/update/${dutyObj.id}`);
+  };
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
+        <Dropdown align="end">
+          <Dropdown.Toggle variant="link" bsPrefix="p-0 border-0 btn" id="ellipsis-dropdown">
+            <BsThreeDotsVertical size={20} />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={handleEdit}>
+              <FaEdit className="me-2" />
+              Edit
+            </Dropdown.Item>
+            <Dropdown.Item onClick={onDelete}>
+              <FaTrash className="me-2 text-danger" />
+              Delete
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <Card.Title>{dutyObj.title}</Card.Title>
+        <hr />
         <Card.Subtitle className="mb-2 text-muted">{dutyObj.dueDate}</Card.Subtitle>
         <Card.Text>{dutyObj.description}</Card.Text>
       </Card.Body>
@@ -15,6 +47,7 @@ function DutyCard({ dutyObj }) {
 
 DutyCard.propTypes = {
   dutyObj: PropTypes.shape({
+    id: PropTypes.number,
     title: PropTypes.string,
     description: PropTypes.string,
     isCompleted: PropTypes.bool,
