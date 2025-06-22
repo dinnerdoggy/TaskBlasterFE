13993557ba30815e488d640b153189f2c4b720d9
Casey <dinnerdoggy@gmail.com>
2025-06-21 22:52:40 -0500

    READ

--- src/app/category/[id]/page.js
+++ src/app/category/[id]/page.js
@@ -8,13 +8,11 @@ import { useAuth } from '../../../utils/context/authContext';
 import { getSingleCategory } from '../../../api/categoryData';
 
 function DutyByCategoryPage({ params }) {
-  // Destructure category ID from dynamic route params
   const { id } = params;
   const { user } = useAuth();
   const [duties, setDuties] = useState([]);
   const [category, setCategory] = useState('...Loading');
 
-  // Create a function to fetch duties by category
   const getAllDutiesByCategory = () => {
     getDutiesByCat(id, user.uid).then((data) => {
       console.log('API call response: ', data);
@@ -30,7 +28,6 @@ function DutyByCategoryPage({ params }) {
     getCurrentCategory();
   }, []);
 
-  // Call function when component mounts or when ID changes
   useEffect(() => {
     getAllDutiesByCategory();
   }, [id]);
@@ -39,7 +36,6 @@ function DutyByCategoryPage({ params }) {
     <div className="dutiesPage">
       <h1 className="mb-4 categoryTitle">{category.title}</h1>
       <div className="mainPage">
-        {/* map over duties here using DutyCard component */}
         {duties.map((duty) => (
           <DutyCard key={duty.id} dutyObj={duty} onUpdate={getAllDutiesByCategory} />
         ))}
